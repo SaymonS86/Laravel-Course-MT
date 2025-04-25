16a7a07d393de96e457fdea190719cd585ec3d70
Bekn The programmer <vp485454@gmail.com>
2025-04-24 22:00:05 -0300

    Update 3 CSS 24/04 Dark Form Login

--- resources/views/events/createvents.blade.php
+++ resources/views/events/createvents.blade.php
@@ -7,10 +7,10 @@
 
 
     <div id="event-create-conatiner" class="col-md-6 offset-md-3">
-        <h1>Crie seu evento</h1>
         <form action="/events" method="POST">
             @csrf
             <div class="form-group">
+                <h1 id="Create-event-title">Crie seu evento:</h1>
                 <label for="title">Evento:</label>
                 <input type="text" class="form-control" id="title" name="title" placeholder="Nome do Enevnto a criar">
 
@@ -38,7 +38,9 @@
                 <label for="title">Descrição:</label>
                 <textarea name="description" id="description" class="form-control" placeholder="O que vai acontecer no evento?"></textarea>
             </div>
-            <input type="submit" class="btn btn-primary" value="Criar Evento">
+            <div id="submit-create-event">
+                <input type="submit" class="btn-primary" value="Criar Evento">
+            </div>
         </form>
     </div>
     <div>
